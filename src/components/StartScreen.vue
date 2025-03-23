92b866b212d409a2c4277f65e0d2ba8011d252f1
poorva-r <poorva.ramnani@gmail.com>
2025-03-23 18:58:02 +0400

    style: update UI

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <div>Start Screen</div>
-    <button @click="startGame">Start Game</button>
+  <div class="bg-background h-screen flex flex-col items-center justify-center">
+    <div class="font-heading text-text text-4xl mb-10">Sudoku</div>
+    <button
+      @click="startGame"
+      class="font-body font-bold text-background text-2xl flex justify-center gap-2 items-center mx-auto shadow-xl bg-primary backdrop-blur-md isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-[#FFFFFF] hover:text-background before:-z-10 before:aspect-square before:hover:scale-200 before:hover:duration-500 relative z-10 px-8 py-2 overflow-hidden border-2 rounded-full group"
+      type="submit"
+    >
+      Start
+    </button>
   </div>
 </template>
 <script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,32 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#e4fcfb',
+        'background': '#021718',
+        'primary': '#8af4ef',
+        'secondary': '#0e2f90',
+        'accent': '#2d34eb',
+       },
+       fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: 'Timmana',
+        body: 'Timmana',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
+    },
   },
   plugins: [],
 }
